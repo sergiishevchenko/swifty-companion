9fbf25881211eb5b1415fa2ac675b3a3439ac329
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-12-05 13:28:35 +0100

    add experimental material API usage in ProfileScreen

--- app/src/main/java/com/students42/app/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/students42/app/ui/profile/ProfileScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -45,6 +46,7 @@ import com.students42.app.ui.components.ProjectsList
 import com.students42.app.ui.components.SkillsList
 import com.students42.app.ui.components.UserInfoCard
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     login: String,
