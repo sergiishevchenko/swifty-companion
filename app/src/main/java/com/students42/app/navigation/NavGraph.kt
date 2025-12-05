fd046f42480a760220948ef122b0983e851d7fd1
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-12-05 17:26:49 +0100

    pass initialUri to LoginScreen in NavGraph

--- app/src/main/java/com/students42/app/navigation/NavGraph.kt
+++ app/src/main/java/com/students42/app/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.students42.app.navigation
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -9,13 +10,16 @@ import com.students42.app.ui.login.LoginScreen
 import com.students42.app.ui.profile.ProfileScreen
 
 @Composable
-fun NavGraph(navController: NavHostController = rememberNavController()) {
+fun NavGraph(
+    navController: NavHostController = rememberNavController(),
+    initialUri: Uri? = null
+) {
     NavHost(
         navController = navController,
         startDestination = "login"
     ) {
         composable("login") {
-            LoginScreen(navController = navController)
+            LoginScreen(navController = navController, initialUri = initialUri)
         }
         composable("profile/{login}") { backStackEntry ->
             val login = backStackEntry.arguments?.getString("login") ?: ""
