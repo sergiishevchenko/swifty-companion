414680342bf667c0ba265e2cc3eaa75582161f4b
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2026-01-16 20:04:28 +0100

    remove unused import in MainActivity

--- app/src/main/java/com/students42/app/MainActivity.kt
+++ app/src/main/java/com/students42/app/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.students42.app
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
